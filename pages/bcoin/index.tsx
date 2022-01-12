4789c5e2e09e924298e018085a28fd81427698c0
Jiraspiom <jiraspiom@yahoo.com.br>
2022-01-12 16:11:48 -0300

    ajuste

--- pages/bcoin/index.tsx
+++ pages/bcoin/index.tsx
@@ -13,6 +13,9 @@ const Home: NextPage = () => {
 
 const gbs = async () =>{
     console.log("calculando")
+
+    const url_brl = "https://api.coingecko.com/api/v3/simple/price?ids=bomber-coin&vs_currencies=brl"
+    const url_usd = "https://api.coingecko.com/api/v3/simple/price?ids=bomber-coin&vs_currencies=usd"
     
     const bcoins: number = 58.64
     var saque: number = 0.97
@@ -35,6 +38,11 @@ const gbs = async () =>{
     console.log(`valor bruto: ${carteriaBcoin * real}, taxa: ${taxa * real}, valor liquido: ${total * real} `)
     console.log(`valor bruto: ${carteriaBcoin * dolar}, taxa: ${taxa * dolar}, valor liquido: ${total * dolar} `)
     
+
+    "https://api.bscscan.com/api?module=account&action=balance&address=0x64a63D24234b2Fd661682A9d5915838F632a99a5&apikey=CF5QKMTA5QHSRY7JPJKPSWCB4Y474518M3"
+    "https://api.bscscan.com/api?module=account&action=txlist&address=0x64a63D24234b2Fd661682A9d5915838F632a99a5&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey=CF5QKMTA5QHSRY7JPJKPSWCB4Y474518M3"
+    "https://api.bscscan.com/api?module=account&action=tokentx&contractaddress=0x00e1656e45f18ec6747F5a8496Fd39B50b38396D&address=0x64a63D24234b2Fd661682A9d5915838F632a99a5&page=1&offset=5&startblock=0&endblock=999999999&sort=asc&apikey=CF5QKMTA5QHSRY7JPJKPSWCB4Y474518M3"
+
 
 }
 
